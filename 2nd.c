63d122faff190784ef6f2d3e23683fea93da71f4
misbah9105 <72156723+misbah9105@users.noreply.github.com>
2020-12-12 10:21:18 +0530

    Add files via upload

---
+++ 2nd.c
@@ -0,0 +1,10 @@
+//onlinr c computer is run c online.
+//writen c code in this online editor and run it.
+
+#include <stdio.h>
+int main() {
+	//printf() displays the string quotation
+	printf("hellow,world");
+	return 0;
+	
+}
